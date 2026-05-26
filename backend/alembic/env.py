dddc7860b66c8fc808c38cf377226fc777d8088e
021divya <btbtc23129_divya@banasthali.in>
2026-05-26 13:43:18 +0530

    Update env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -10,22 +10,14 @@ from alembic import context
 config = context.config
 
 # Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
+
 from app.core.database import Base
 from app.models import user
 
 target_metadata = Base.metadata
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
